7c477fbed5adeea4a07ef7538e7152384adb5e64
roberto <robpalacios1@hotmail.com>
2026-04-30 14:28:03 -0500

    add four resouces block for NAT Gateway AZ1/AZ2 and for EIP AZ1/AZ2

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -2,7 +2,7 @@
 resource "aws_vpc" "main_vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name = var.vpc_name
+    Name        = var.vpc_name
     environment = var.vpc_environment
   }
 }
@@ -11,53 +11,53 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_internet_gateway" "main_internet_gateway" {
   vpc_id = aws_vpc.main_vpc.id
   tags = {
-    Name = var.internet_gateway_name
+    Name        = var.internet_gateway_name
     environment = var.vpc_environment
   }
 }
 
 #3. Create a public subnet 1
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id = aws_vpc.main_vpc.id
-  cidr_block = var.public_subnet_1_cidr_block
-  availability_zone = var.availability_zone_1
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.public_subnet_1_cidr_block
+  availability_zone       = var.availability_zone_1
   map_public_ip_on_launch = var.public_subnet_map_public_ip_on_launch
   tags = {
-    Name = var.public_subnet_1_name
+    Name        = var.public_subnet_1_name
     environment = var.vpc_environment
   }
 }
 
 #4. Create a public subnet 2
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id = aws_vpc.main_vpc.id
-  cidr_block = var.public_subnet_2_cidr_block
-  availability_zone = var.availability_zone_2
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = var.public_subnet_2_cidr_block
+  availability_zone       = var.availability_zone_2
   map_public_ip_on_launch = var.public_subnet_map_public_ip_on_launch
   tags = {
-    Name = var.public_subnet_2_name
+    Name        = var.public_subnet_2_name
     environment = var.vpc_environment
   }
 }
 
 #5. Create a private subnet 1 for the application
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id = aws_vpc.main_vpc.id
-  cidr_block = var.private_subnet_1_cidr_block
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.private_subnet_1_cidr_block
   availability_zone = var.availability_zone_1
   tags = {
-    Name = var.private_subnet_1_name
+    Name        = var.private_subnet_1_name
     environment = var.vpc_environment
   }
 }
 
 #6. Create a private subnet 2 for the database
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id = aws_vpc.main_vpc.id
-  cidr_block = var.private_subnet_2_cidr_block
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = var.private_subnet_2_cidr_block
   availability_zone = var.availability_zone_2
   tags = {
-    Name = var.private_subnet_2_name
+    Name        = var.private_subnet_2_name
     environment = var.vpc_environment
   }
 }
@@ -66,26 +66,66 @@ resource "aws_subnet" "private_subnet_2" {
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main_vpc.id
   tags = {
-    Name = var.public_route_table_name
+    Name        = var.public_route_table_name
     environment = var.vpc_environment
   }
 }
 
 #8. Create a route table destination CIDR block
 resource "aws_route" "public_internet_access" {
-  route_table_id = aws_route_table.public_route_table.id
+  route_table_id         = aws_route_table.public_route_table.id
   destination_cidr_block = var.public_route_destination_cidr_block
-  gateway_id = aws_internet_gateway.main_internet_gateway.id
+  gateway_id             = aws_internet_gateway.main_internet_gateway.id
 }
 
 #9 Create a route table Association for subnet 1
 resource "aws_route_table_association" "public_subnet_1_association" {
-  subnet_id = aws_subnet.public_subnet_1.id
+  subnet_id      = aws_subnet.public_subnet_1.id
   route_table_id = aws_route_table.public_route_table.id
 }
 
 #9 Create a route table Association for subnet 2
 resource "aws_route_table_association" "public_subnet_2_association" {
-  subnet_id = aws_subnet.public_subnet_2.id
+  subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_route_table.id
+}
+
+#10. Elastic IPs for NAT1
+resource "aws_eip" "nat_eip_az1" {
+  domain = "vpc"
+  tags = {
+    Name        = var.nat_eip_az1
+    environment = var.vpc_environment
+  }
+}
+
+#11. Elastic IPs for NAT2
+resource "aws_eip" "nat_eip_az2" {
+  domain = "vpc"
+  tags = {
+    Name        = var.nat_eip_az2
+    environment = var.vpc_environment
+  }
+}
+
+#12. NAT Gateway on public subnet 1(AZ1)
+resource "aws_nat_gateway" "aws_nat_gateway_az1" {
+  allocation_id = aws_eip.nat_eip_az1.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+  tags = {
+    Name        = var.nat-gateway-az1
+    environment = var.vpc_environment
+  }
+  depends_on = [aws_internet_gateway.main_internet_gateway]
+}
+
+#12. NAT Gateway on public subnet 2(AZ2)
+resource "aws_nat_gateway" "aws_nat_gateway_az2" {
+  allocation_id = aws_eip.nat_eip_az2.id
+  subnet_id     = aws_subnet.public_subnet_2.id
+  tags = {
+    Name        = var.nat-gateway-az2
+    environment = var.vpc_environment
+  }
+  depends_on = [aws_internet_gateway.main_internet_gateway]
 }
