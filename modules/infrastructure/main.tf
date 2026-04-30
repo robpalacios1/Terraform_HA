3986883f51cd771dfab0357e28d3da3079bf8d3b
roberto <robpalacios1@hotmail.com>
2026-04-30 17:19:06 -0500

    add more resources

--- modules/infrastructure/main.tf
+++ modules/infrastructure/main.tf
@@ -139,21 +139,7 @@ resource "aws_route_table" "private_route_table_az1" {
   }
 }
 
-#15. Default Route Private AZ1 -> NAT AZ1
-resource "aws_route" "private_default_route_az1" {
-  route_table_id         = aws_route_table.private_route_table_az1.id
-  destination_cidr_block = var.public_route_destination_cidr_block
-  nat_gateway_id         = aws_nat_gateway.aws_nat_gateway_az1.id
-}
-
-#16. Default Route Private AZ2 -> NAT AZ2
-resource "aws_route" "private_default_route_az2" {
-  route_table_id         = aws_route_table.private_route_table_az2.id
-  destination_cidr_block = var.public_route_destination_cidr_block
-  nat_gateway_id         = aws_nat_gateway.aws_nat_gateway_az2.id
-}
-
-#17. private Route Table AZ2
+#15. private Route Table AZ2
 resource "aws_route_table" "private_route_table_az2" {
   vpc_id = aws_vpc.main_vpc.id
   tags = {
@@ -162,14 +148,28 @@ resource "aws_route_table" "private_route_table_az2" {
   }
 }
 
+#16. Default Route Private AZ1 -> NAT AZ1
+resource "aws_route" "private_default_route_az1" {
+  route_table_id         = aws_route_table.private_route_table_az1.id
+  destination_cidr_block = var.public_route_destination_cidr_block
+  nat_gateway_id         = aws_nat_gateway.aws_nat_gateway_az1.id
+}
+
+#17. Default Route Private AZ2 -> NAT AZ2
+resource "aws_route" "private_default_route_az2" {
+  route_table_id         = aws_route_table.private_route_table_az2.id
+  destination_cidr_block = var.public_route_destination_cidr_block
+  nat_gateway_id         = aws_nat_gateway.aws_nat_gateway_az2.id
+}
+
 #18. Association of Privates Subnet 1
 resource "aws_route_table_association" "private_subnet_1_association" {
   subnet_id      = aws_subnet.private_subnet_1.id
   route_table_id = aws_route_table.private_route_table_az1.id
 }
 
-#19. Association of Privates Subnet 1
+#19. Association of Privates Subnet 2
 resource "aws_route_table_association" "private_subnet_2_association" {
-  subnet_id      = aws_subnet.private_subnet_1.id
+  subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_route_table_az2.id
 }
