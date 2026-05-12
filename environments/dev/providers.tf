92b526f5206df4c458595b7e39eb1ba0b3b3368c
roberto <robpalacios1@hotmail.com>
2026-05-12 12:09:59 -0500

    create backend s3 into providers.tf

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "my-project-tf-state-us-east-1"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-ha-dev-locks"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
